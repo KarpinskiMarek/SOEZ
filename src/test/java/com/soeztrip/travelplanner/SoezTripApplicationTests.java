ce6abf7df687c11e01c69175207580c9ecd3bfdb
BigChungus2137 <marek.karpinski96@gmail.com>
2023-09-04 19:05:36 +0200

    first commit

---
+++ src/test/java/com/soeztrip/travelplanner/SoezTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soeztrip.travelplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoezTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
