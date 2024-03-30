1114796583d95d2ff10896b7f14cf8a6be3eec30
KTokarski2 <krzysztof.tokarski2@icloud.com>
2024-03-30 01:32:58 +0100

    create trips list/to fix

--- soez_react/src/components/Trips.jsx
+++ soez_react/src/components/Trips.jsx
@@ -1,22 +1,70 @@
 import styled from "styled-components";
 import React from "react";
+import LinkButton from "./LinkButton";
+import TravelTile from "./TravelTile";
 
 
-const StyledTripsDiv = styled.div`
+const TripsDiv = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 200vh;
-  margin: auto;
-  background-color: black;
+  min-height: 100vh;
+  max-width: 95%;
+  background-color: white;
+  margin: 2rem auto;
+  border-radius: 5px;
+  padding: 2rem;
+`;
+
+const TilesList = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 75%;
+`;
+
+const CreateButton = styled(LinkButton)`
+  width: 600px;
+  margin: 5rem;
 `;
 
 const Trips = () => {
     return(
-        <StyledTripsDiv>
-
-        </StyledTripsDiv>
+        <TripsDiv>
+            <CreateButton buttonText={"Utwórz podróż"}/>
+            <TilesList>
+                <TravelTile title={"Włochy 2024"} dateFrom={"21.07.2024"} dateTo={"30.07.2024"}/>
+                <TravelTile title={"Jesien Hiszpania"} dateFrom={"11.09.2024"} dateTo={"18.09.2024"}/>
+                <TravelTile title={"Narty Austria"} dateFrom={"02.02.204"} dateTo={"12.02.2024"}/>
+                <TravelTile title={"Majówka"} dateFrom={"01.05.2024"} dateTo={"05.05.2024"}/>
+                <TravelTile title={"Włochy 2024"} dateFrom={"21.07.2024"} dateTo={"30.07.2024"}/>
+                <TravelTile title={"Jesien Hiszpania"} dateFrom={"11.09.2024"} dateTo={"18.09.2024"}/>
+                <TravelTile title={"Narty Austria"} dateFrom={"02.02.204"} dateTo={"12.02.2024"}/>
+                <TravelTile title={"Majówka"} dateFrom={"01.05.2024"} dateTo={"05.05.2024"}/>
+                <TravelTile title={"Włochy 2024"} dateFrom={"21.07.2024"} dateTo={"30.07.2024"}/>
+                <TravelTile title={"Jesien Hiszpania"} dateFrom={"11.09.2024"} dateTo={"18.09.2024"}/>
+                <TravelTile title={"Narty Austria"} dateFrom={"02.02.204"} dateTo={"12.02.2024"}/>
+                <TravelTile title={"Majówka"} dateFrom={"01.05.2024"} dateTo={"05.05.2024"}/>
+                <TravelTile title={"Włochy 2024"} dateFrom={"21.07.2024"} dateTo={"30.07.2024"}/>
+                <TravelTile title={"Jesien Hiszpania"} dateFrom={"11.09.2024"} dateTo={"18.09.2024"}/>
+                <TravelTile title={"Narty Austria"} dateFrom={"02.02.204"} dateTo={"12.02.2024"}/>
+                <TravelTile title={"Majówka"} dateFrom={"01.05.2024"} dateTo={"05.05.2024"}/>
+                <TravelTile title={"Włochy 2024"} dateFrom={"21.07.2024"} dateTo={"30.07.2024"}/>
+                <TravelTile title={"Jesien Hiszpania"} dateFrom={"11.09.2024"} dateTo={"18.09.2024"}/>
+                <TravelTile title={"Narty Austria"} dateFrom={"02.02.204"} dateTo={"12.02.2024"}/>
+                <TravelTile title={"Majówka"} dateFrom={"01.05.2024"} dateTo={"05.05.2024"}/>
+                <TravelTile title={"Włochy 2024"} dateFrom={"21.07.2024"} dateTo={"30.07.2024"}/>
+                <TravelTile title={"Jesien Hiszpania"} dateFrom={"11.09.2024"} dateTo={"18.09.2024"}/>
+                <TravelTile title={"Narty Austria"} dateFrom={"02.02.204"} dateTo={"12.02.2024"}/>
+                <TravelTile title={"Majówka"} dateFrom={"01.05.2024"} dateTo={"05.05.2024"}/>
+                <TravelTile title={"Włochy 2024"} dateFrom={"21.07.2024"} dateTo={"30.07.2024"}/>
+                <TravelTile title={"Jesien Hiszpania"} dateFrom={"11.09.2024"} dateTo={"18.09.2024"}/>
+                <TravelTile title={"Narty Austria"} dateFrom={"02.02.204"} dateTo={"12.02.2024"}/>
+                <TravelTile title={"Majówka"} dateFrom={"01.05.2024"} dateTo={"05.05.2024"}/>
+            </TilesList>
+        </TripsDiv>
     )
 }
 
